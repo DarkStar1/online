4616286265011df65b36c848e075c5265beffdb5
Lanre <el_gigantes@hotmail.co.uk>
2017-07-25 13:17:27 +0200

    no message

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -435,8 +435,9 @@ bool DocumentBroker::load(const std::shared_ptr<ClientSession>& session, const s
 
         if (!wopifileinfo->_userCanWrite || LOOLWSD::IsViewFileExtension(wopiStorage->getFileExtension()))
         {
-            LOG_DBG("\nUserCanWrite: " + wopifileinfo->_userCanWrite.toString());
-            LOG_DBG("\nIsViewFileExtension: " + LOOLWSD::IsViewFileExtension(wopiStorage->getFileExtension().toString()));
+            LOG_DBG("\nUserCanWrite: " + (wopifileinfo->_userCanWrite) ? "true" : "false";
+            LOG_DBG("\nFileExtension: " + wopiStorage->getFileExtension() );
+            // LOG_DBG("\nIsViewFileExtension: " + LOOLWSD::IsViewFileExtension(wopiStorage->getFileExtension()));
             LOG_DBG("\n\nSetting the session as readonly");
             session->setReadOnly();
         }
